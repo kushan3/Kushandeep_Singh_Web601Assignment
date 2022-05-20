6d2bf5c9d48c0537cfd7deea3d73800319a00991
kushan3 <w0777326@myscc.ca>
2022-05-20 14:25:03 -0400

    Set the version 0.2.0  in package.json file and Add the contentLisrt file

--- KSinghMyFavouritePunjabiMovies/src/app/app.module.ts
+++ KSinghMyFavouritePunjabiMovies/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentListComponent
   ],
   imports: [
     BrowserModule
