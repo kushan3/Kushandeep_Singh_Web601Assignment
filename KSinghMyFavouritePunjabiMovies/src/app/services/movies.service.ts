816325287a5db55692c35a0760bf44c28b0f2bfc
kushan3 <w0777326@myscc.ca>
2022-06-15 12:41:58 -0400

    create method in services

--- KSinghMyFavouritePunjabiMovies/src/app/services/movies.service.ts
+++ KSinghMyFavouritePunjabiMovies/src/app/services/movies.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Content} from "../models/content";
+import {Observable, of} from "rxjs";
+import {MYLIST} from "../data/mock-content-db";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,21 @@ import { Injectable } from '@angular/core';
 export class MoviesService {
 
   constructor() { }
+  getContent(): Observable<Content[]> {
+    return of(MYLIST);
+  }
+  getOneContent(id: number): Observable<Content[]> {
+    return of(MYLIST);
+  }
+  addContent(newContent: Content): Observable<Content[]> {
+    return of(MYLIST);
+  }
+  updateContent(newContent: Content): Observable<Content[]> {
+    return of(MYLIST);
+  }
+  removeContent(id: number): Observable<Content[]> {
+    return of(MYLIST);
+  }
+
+
 }
