ad4c4fac68e0e142949b5ae802f4c7fa23fa76d4
kushan3 <w0777326@myscc.ca>
2022-07-24 05:57:23 -0400

    Change all of the inputs and buttons

--- KSinghMyFavouritePunjabiMovies/src/app/app.module.ts
+++ KSinghMyFavouritePunjabiMovies/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { ChangeContentComponent } from './change-content/change-content.componen
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,6 +36,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       delay: 1000,
     }),
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
